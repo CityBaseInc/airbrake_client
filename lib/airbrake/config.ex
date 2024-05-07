cbcc5a5b26b77714e3b574d914a0a31ad4a45176
Jeremy D. Frens <jeremy.frens@eunasolutions.com>
2024-05-07 17:01:48 -0500

    translate context_environment to "production" using aliases
    The :production_aliases can be used to translate the context environment
    to "production" (which has special meaning to Airbrake).

--- lib/airbrake/config.ex
+++ lib/airbrake/config.ex
@@ -26,13 +26,18 @@ defmodule Airbrake.Config do
   # Returns the name of the environment.
   @impl Airbrake.Config.Behaviour
   def context_environment(config \\ __MODULE__) do
-    case config.get(:context_environment) || config.get(:environment) do
-      nil -> hostname()
-      {:system, var} -> System.get_env(var, hostname())
-      atom_env when is_atom(atom_env) -> to_string(atom_env)
-      str_env when is_binary(str_env) -> str_env
-      fun_env when is_function(fun_env) -> fun_env.()
-    end
+    config_context_environment =
+      case config.get(:context_environment) || config.get(:environment) do
+        nil -> hostname()
+        {:system, var} -> System.get_env(var, hostname())
+        atom_env when is_atom(atom_env) -> to_string(atom_env)
+        str_env when is_binary(str_env) -> str_env
+        fun_env when is_function(fun_env) -> fun_env.()
+      end
+
+    if config_context_environment in config.get(:production_aliases, []),
+      do: "production",
+      else: config_context_environment
   end
 
   # Returns a hostname.
--- test/airbrake/config_test.exs
+++ test/airbrake/config_test.exs
@@ -14,10 +14,12 @@ defmodule Airbrake.ConfigTest do
       check all context_environment <- context_environment(),
                 environment <- context_environment(),
                 context_environment != environment do
-        stub(MockConfig, :get, fn
+        MockConfig
+        |> stub(:get, fn
           :environment -> environment
           :context_environment -> context_environment
         end)
+        |> stub(:get, fn :production_aliases, [] -> [] end)
 
         assert Config.context_environment(MockConfig) == context_environment
         assert Config.context_environment(MockConfig) != environment
@@ -26,13 +28,42 @@ defmodule Airbrake.ConfigTest do
 
     property "returns string from :environment of config" do
       check all environment <- context_environment() do
-        stub(MockConfig, :get, fn
+        MockConfig
+        |> stub(:get, fn
           :environment -> environment
           :context_environment -> nil
         end)
+        |> stub(:get, fn :production_aliases, [] -> [] end)
 
         assert Config.context_environment(MockConfig) == environment
       end
     end
+
+    property "translates production_aliases to production" do
+      check all production_environments <- list_of(random_environment(), min_length: 10),
+                context_environment <- member_of(production_environments) do
+        MockConfig
+        |> stub(:get, fn :context_environment -> context_environment end)
+        |> stub(:get, fn :production_aliases, [] -> production_environments end)
+
+        assert Config.context_environment(MockConfig) == "production"
+      end
+    end
+
+    property "no translation if not in production_aliases" do
+      check all production_environments <- list_of(random_environment(), min_length: 10),
+                context_environment <- random_environment(),
+                context_environment not in production_environments do
+        MockConfig
+        |> stub(:get, fn :context_environment -> context_environment end)
+        |> stub(:get, fn :production_aliases, [] -> production_environments end)
+
+        assert Config.context_environment(MockConfig) == context_environment
+      end
+    end
+  end
+
+  defp random_environment do
+    string(:alphanumeric, min_length: 1)
   end
 end

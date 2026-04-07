b85685ad1b87a0c49af66446fbad3dbcdfd59c05
Jeremy D. Frens <jeremy.frens@eunasolutions.com>
2026-04-07 13:08:12 -0500

    handle IPv4 and IPv6 for userIP in Aibrake.Plug

--- lib/airbrake/plug.ex
+++ lib/airbrake/plug.ex
@@ -25,7 +25,7 @@ defmodule Airbrake.Plug do
 
         conn_data = %{
           url: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
-          userIP: conn.remote_ip |> Tuple.to_list() |> Enum.join("."),
+          userIP: conn.remote_ip |> :inet.ntoa() |> to_string(),
           userAgent: headers["user-agent"],
           cookies: conn.req_cookies
         }

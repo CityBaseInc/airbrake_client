5eaedbff307dd78e4c92c3ea9c9b8b7d7ce5e082
Jeremy D. Frens <jeremy.frens@eunasolutions.com>
2026-04-07 12:30:32 -0500

    add Aibrake.Test, rename Aibrake.HTTPMock to Airbrake.MockHTTPoison

--- config/test.exs
+++ config/test.exs
@@ -4,5 +4,5 @@ import Config
 config :airbrake_client,
   api_key: "TESTING_API_KEY",
   project_id: 8_675_309,
-  private: [http_adapter: Airbrake.HTTPMock],
+  private: [http_adapter: Airbrake.MockHTTPoison],
   filter_parameters: ["password"]
